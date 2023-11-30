17f13297ddde1065868e1cbc7fd037f1459528a2
safdar211ali <safdar211@hotmail.com>
2023-11-30 22:55:43 +0500

    udated

--- mehran_reports/mehran_reports/report/sales_order_pending_item_report/sales_order_pending_item_report.py
+++ mehran_reports/mehran_reports/report/sales_order_pending_item_report/sales_order_pending_item_report.py
@@ -53,6 +53,11 @@ def get_columns():
             "fieldname": "so_qty",
             "fieldtype": "Data",
             "width": 150
+        },{
+            "label": _("Nos"),
+            "fieldname": "nos",
+            "fieldtype": "Data",
+            "width": 150
         }
         ,
         {
@@ -94,6 +99,7 @@ def get_data(filters):
             `tabSales Order`.name AS sales_order_no,
             `tabSales Order Item`.item_code,
             `tabSales Order Item`.description,
+            `tabSales Order Item`.nos,
             SUM(`tabSales Order Item`.qty) AS so_qty,
             (
                 SELECT COALESCE(SUM(`tabDelivery Note Item`.qty),0)
